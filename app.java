4f8533998cb090b94fce0d00332b1fbb420a81ab
Anasya Miftakus <100329265+anasyaams@users.noreply.github.com>
2023-01-16 14:29:41 +0700

    Create app.java

---
+++ app.java
@@ -0,0 +1,8 @@
+import java.text.ParseException;
+
+public class app {
+    public static void main(String[] args) throws ParseException{
+        taskController control = new taskController();
+        control.jalan();  
+    }
+}
